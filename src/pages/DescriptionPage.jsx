06d35cc72ba57b167bf66339dca960c641473f21
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2024-10-12 15:50:13 +0100

    react routing

--- src/pages/DescriptionPage.jsx
+++ src/pages/DescriptionPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const DescriptionPage = ({ movies }) => {
   const { title } = useParams();
   const movie = movies.find((movie) => movie.title === title);
   return (
     <div>
+      <Link to="/">
+        <IoMdArrowRoundBack />
+      </Link>
       <h1>{movie.title}</h1>
       <img src={movie.posterURL} alt={movie.title} />
       <h3>Movie Description</h3>
--- src/pages/TrailerPage.jsx
+++ src/pages/TrailerPage.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const TrailerPage = ({ movies }) => {
   const { title } = useParams();
   const movie = movies.find((movie) => movie.title === title);
   return (
-    <iframe
-      width="560"
-      height="315"
-      src={movie.trailerLink}
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin"
-      allowfullscreen
-    ></iframe>
+    <Container>
+      <Link to="/">
+        <IoMdArrowRoundBack />
+      </Link>
+      <iframe
+        width="560"
+        height="315"
+        src={movie.trailerLink}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </Container>
   );
 };
 
